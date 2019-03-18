3382d58d0035923094f6ce319de44b7dca82670b
Arivin Nair <arivin.nair@gmail.com>
2019-03-18 10:11:52 -0700

    Modified the original app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component{
+    render(){
+        return( 
+            <div class="App">
+                This is my app
+            </div>
+        )
+    }
+}
+
